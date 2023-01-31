4c714b1c8cadcd1b8c11e5856956ff6b175cdde5
Alejo Boga <alejoboga20@gmail.com>
2023-01-31 16:13:19 -0500

    Create post with action

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -7,3 +7,7 @@ export const getPosts = async () => {
 export const getPost = async (slug: string) => {
   return prisma.post.findUnique({ where: { slug } });
 };
+
+export const createPost = async (post) => {
+  return prisma.post.create({ data: post });
+};
--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,5 +1,57 @@
+import { Form } from "@remix-run/react";
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { createPost } from "~/models/post.server";
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  await createPost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+};
+
 const NewPost = () => {
-  return <h2>New Post</h2>;
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title:{" "}
+          <input type="text" name="title" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug:{" "}
+          <input type="text" name="slug" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">Markdown:</label>
+        <br />
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+        />
+      </p>
+      <div className="text-right">
+        <button
+          type="submit"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+        >
+          Create Post
+        </button>
+      </div>
+    </Form>
+  );
 };
 
 export default NewPost;
