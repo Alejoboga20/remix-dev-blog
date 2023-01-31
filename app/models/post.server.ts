29da5e5b8f64b728f4ef4736e8de9926958ec62c
Alejo Boga <alejoboga20@gmail.com>
2023-01-31 15:26:16 -0500

    Get posts from DB

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,5 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { prisma } from "~/db.server";
 
-export const getPosts = async (): Promise<Post[]> => {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+export const getPosts = async () => {
+  return prisma.post.findMany();
 };
