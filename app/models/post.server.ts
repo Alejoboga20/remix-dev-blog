ccd19b1e8f6d11e55118f4e3c0eca977dd1a1f24
Alejo Boga <alejoboga20@gmail.com>
2023-01-31 16:27:25 -0500

    Add type safety for actions

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,4 +1,5 @@
 import { prisma } from "~/db.server";
+import type { Post } from "@prisma/client";
 
 export const getPosts = async () => {
   return prisma.post.findMany();
@@ -8,6 +9,8 @@ export const getPost = async (slug: string) => {
   return prisma.post.findUnique({ where: { slug } });
 };
 
-export const createPost = async (post) => {
+export const createPost = async (
+  post: Pick<Post, "slug" | "title" | "markdown">
+) => {
   return prisma.post.create({ data: post });
 };
--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,7 +1,9 @@
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import type { ActionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
@@ -12,28 +14,56 @@ export const action = async ({ request }: ActionArgs) => {
   const slug = formData.get("slug");
   const markdown = formData.get("markdown");
 
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
+
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
+
   await createPost({ title, slug, markdown });
 
   return redirect("/posts/admin");
 };
 
 const NewPost = () => {
+  const errors = useActionData<typeof action>();
+
   return (
     <Form method="post">
       <p>
         <label>
           Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
           <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
           Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
           <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
-        <label htmlFor="markdown">Markdown:</label>
+        <label htmlFor="markdown">
+          Markdown:
+          {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}
+        </label>
         <br />
         <textarea
           id="markdown"
