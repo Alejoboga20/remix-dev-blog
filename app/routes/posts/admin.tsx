bb0aea4c30d47cc77d1e2373e229a6d7259bed31
Alejo Boga <alejoboga20@gmail.com>
2023-01-31 16:06:31 -0500

    Nested routes

--- app/routes/posts/admin.tsx
+++ app/routes/posts/admin.tsx
@@ -1,6 +1,6 @@
 import { json } from "@remix-run/node";
 import { getPosts } from "~/models/post.server";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 export const loader = async () => {
   return json({ posts: await getPosts() });
@@ -24,7 +24,9 @@ const PostAdmin = () => {
             ))}
           </ul>
         </nav>
-        <main className="col-span-4 md:col-span-3">...</main>
+        <main className="col-span-4 md:col-span-3">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
---
+++ app/routes/posts/admin/new.tsx
@@ -0,0 +1,5 @@
+const NewPost = () => {
+  return <h2>New Post</h2>;
+};
+
+export default NewPost;
