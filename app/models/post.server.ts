39a51bcee90654e07931d9f115b66cdd81b44e22
Alejo Boga <alejoboga20@gmail.com>
2023-01-31 15:46:51 -0500

    Add type safety with invariant

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -3,3 +3,7 @@ import { prisma } from "~/db.server";
 export const getPosts = async () => {
   return prisma.post.findMany();
 };
+
+export const getPost = async (slug: string) => {
+  return prisma.post.findUnique({ where: { slug } });
+};
--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,16 +1,26 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
 
 export const loader = async ({ params }: LoaderArgs) => {
-  return json({ slug: params.slug });
+  invariant(params.slug, "params.slug is required");
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  return json({ post });
 };
 
 const PostSlug = () => {
-  const { slug } = useLoaderData<typeof loader>();
+  const { post } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">Post: {slug}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">
+        Post title: {post?.title}
+      </h1>
+      <code>{post?.markdown}</code>
     </main>
   );
 };
