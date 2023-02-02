6b6eeb56bfde024da1a460b6ebb0f41cbbc21e12
Alejo Boga <alejoboga20@gmail.com>
2023-02-02 09:30:00 -0500

    Embed post in adming panel

---
+++ app/routes/posts/admin/$slug.tsx
@@ -0,0 +1,30 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/post.server";
+import { marked } from "marked";
+import invariant from "tiny-invariant";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.slug, "params.slug is required");
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  const html = marked(post.markdown);
+  return json({ post, html });
+};
+
+const PostSlug = () => {
+  const { post, html } = useLoaderData<typeof loader>();
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">
+        Post title: {post?.title}
+      </h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
+  );
+};
+
+export default PostSlug;
